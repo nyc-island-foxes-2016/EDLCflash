f2fcdb252645034ad351c85b5ec76d1b4d2e0189
Edward <email": null>
2015-12-11 16:18:06 -0500

    Class Deck and Flashcard ready for parser

--- deck.rb
+++ deck.rb
@@ -1,11 +1,15 @@
+require_relative 'flashcard'
+
 class Deck
   # include Parser
-  attr_reader :deck
+  attr_accessor :deck
 
-  def initialize
-    deck = []
-    #deck << FlashCard.new()
+  def initialize(info)
+    @deck = []
+    @deck << info.each { |card_hash| FlashCard.new(card_hash)}
   end
 
-
 end
+driver_code = [{ :term => "swag", :definition => "The action and effect of being too coll to handle"}, { :term => "fresh", :definition => "The effect of being too coll to handle"}]
+my_deck = Deck.new(driver_code)
+p my_deck
