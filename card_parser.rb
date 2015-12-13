a47975382c6d8f9b55bbbd219d2497a84383d25c
Edward <email": null>
2015-12-13 11:45:10 -0500

    up to date progress

--- card_parser.rb
+++ card_parser.rb
@@ -1,12 +1,48 @@
-require_relative
-
+require_relative 'flashcard'
+require 'CSV'
 module Parsable
 
-def parser(csv_file)
-  def list_parser
-    read_csv = CSV.read(csv_file,:headers=>true, :header_converters=>:symbol)
-    read_csv.map{|row| FlashCard.new(row.to_hash)}
+  # def self.list_parser(csv_file)
+  #   read_csv = CSV.read(csv_file,:headers=>true, :header_converters=>:symbol)
+  #   read_csv.map{|row| FlashCard.new(row.to_hash)}
+  # end
+
+  def card_parser(txt_file)
+    read_txt = File.open(txt_file, "r")
+    array = []
+    read_txt.map{|line| array << line}
+    # 'hash_array = []
+    array.each_slice(2){|line| FlashCard.new({definition: line[0], word: line[1]}})
   end
 
+
+
+ def generate_line_pairs_from_txt(file_path)
+    txt_lines = File.read(file_path).split(/\n/)
+    pair_txt_lines(txt_lines)
+  end
+
+  def pair_txt_lines(txt_lines)
+    line_pairs = []
+    txt_lines = txt_lines.reject{|line| line == ""}
+    txt_lines.each_slice(2) {|pair| line_pairs << pair}
+    line_pairs
+  end
 end
 
+# read_txt = File.open("/Users/apprentice/Desktop/EDLCflash/flashcard.txt", "r")
+# array = []
+# read_txt.map{|line| array << line}
+# hash_array = []
+# array.each_slice(2){|line| hash_array << {definition: line[0], word: line[1]}}
+
+
+# read_txt.each_slice(2){|element| element.to_hash}
+
+# element = [ "x", "y"]
+
+# hash.new (definition: element[0], word: element[1])
+
+# FlashCard.new = {definition: element[0], word: element[1]}
+
+# read_txt = File.open(text_file, "r" )
--- view.rb
+++ view.rb
@@ -1,32 +1,41 @@
 class View
   def introduction
-    puts "Welcome to the EDLC flashcard App! We hope you learn something today!"
+    puts "Welcome to the Inner Critic Flashcard App! We hope you learn something today, but if not, we really don't care!"
   end
 
   def user_interface
-    puts "Would you like to go over your flashcards? (y/n)"
+    puts "Would you like to go over your flashcards? (yes/no)"
   end
 
   def stop_playing
-    puts "Are you going to procastinate and stop playing? (y/n)"
+    puts "Are you going to procastinate and stop playing? (quit)"
     puts "Really?"
     puts "But are you really really sure?"
   end
 
+  def question
+    puts "What is (a)#{@deck[0][:term]}?"
+  end
+
   def correct_answer
-    puts "You got it! What a genius!!! (single clap../) Would you like to continue (y/n)"
+    puts "You got it! What a genius!!! (single clap../) Would you like to continue? (continue)"
   end
 
   def wrong_answer
-    puts ":( oh no! You were wrong. Would you like to guess again? (y/n)"
+    puts ":( oh no! You were wrong. Would you like to guess again? (again) Or move on? (move)"
   end
 
   def clear_cards
-    puts "Would you like to clear all your flash cards? (y/n) \n You HAVE to know all this stuff by now."
+    puts "Would you like to clear all your flash cards? (clear) \n You HAVE to know all this stuff by now."
+  end
+
+  def goodbye
+    puts "Sheesh. Didn't expect you to give up THAT easily."
   end
 
   def get_input
-    gets.chomp
+    gets.chomp.downcase
   end
 end
+
 
